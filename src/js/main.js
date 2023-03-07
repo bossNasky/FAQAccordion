8b2d4017a13a4452bf3d5b6de8a27213b65aebf1
bossNasky <naskret.kamil@wp.pl>
2023-03-07 18:02:37 +0100

    Work with JS
    Add a accordion js functionality with accesibility changes.

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,29 @@
 import "./../css/style.css";
+
+const initAccordion = function () {
+  const faqElement = document.querySelector(".faq");
+  const faqItems = document.querySelectorAll(".faq__item");
+  const faqButtons = document.querySelectorAll(".btn--faq");
+
+  const closeFaqs = function () {
+    faqItems.forEach((faqItem) => faqItem.classList.remove("active"));
+    faqButtons.forEach((faqButton) =>
+      faqButton.setAttribute("aria-expanded", false)
+    );
+  };
+
+  const openFaq = function (target) {
+    target.closest(".faq__item").classList.add("active");
+    target.setAttribute("aria-expanded", true);
+  };
+
+  faqElement.addEventListener("click", (ev) => {
+    const target = ev.target;
+    if (target.classList.contains("btn--faq")) {
+      closeFaqs();
+      openFaq(target);
+    }
+  });
+};
+
+initAccordion();
